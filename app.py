969ff61a691dcedd49eb98624fdaa2dcb5f1f58f
crystalmays <chrissymays95@gmail.com>
2022-01-24 23:41:38 -0500

    added marshmallow schemas

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://zkfrmxxojtbsqi:ba584756bd4fd7a33603aaebd054d0e78a13f6f997914bb7b4c0868de366e34d@ec2-3-216-113-109.compute-1.amazonaws.com:5432/d3s3rcomnckh1t"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://zkfrmxxojtbsqi:ba584756bd4fd7a33603aaebd054d0e78a13f6f997914bb7b4c0868de366e34d@ec2-3-216-113-109.compute-1.amazonaws.com:5432/d3s3rcomnckh1t"
 
 
 db = SQLAlchemy(app)
@@ -13,31 +13,50 @@ ma = Marshmallow(app)
 class Month(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable = False)
-    year = db.Column(db.String,) nullable=False)
-    days_in_month = db.Column(db.Integer, nullable=False)
+    year = db.Column(db.String, nullable = False)
+    days_in_month = db.Column(db.Integer, nullable = False)
     days_in_previous_month = db.Column(db.Integer, nullable=False)
-    start_day = db.Column(db.Intger, nullable=False)
+    start_day = db.Column(db.Integer, nullable=False)
 
-    def __init__(self, name, year, days_in_month, days_in_previous_month, start_day)
-    self.name = name
-    self.year = year
-    self.days_in_month = days_in_month
-    self.days_in_previous_month = days_in_previous_month
-    self.start_day = start_day
+    def __init__(self, name, year, days_in_month, days_in_previous_month, start_day):
+        self.name = name
+        self.year = year
+        self.days_in_month = days_in_month
+        self.days_in_previous_month = days_in_previous_month
+        self.start_day = start_day
 
 
-class Reminder(db.Model)
-id = db.Column(db.Integer, primary_key=True)
-day = db.Column(db.String, nullable = False)
-month = db.Column(db.String, nullable = False)
-year = db.Column(db.String, nullable = False)
-text = db.Column(db.String, nullable = False)
+class Reminder(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    day = db.Column(db.String, nullable = False)
+    month = db.Column(db.String, nullable = False)
+    year = db.Column(db.String, nullable = False)
+    text = db.Column(db.String, nullable = False)
 
-def __init__(self,day,month,year,text):
+def __init__(self, day, month, year, text):
     self.day = day
     self.month = month
     self.year = year
     self.text = text 
+
+class MonthSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "name", "year","days_in_month", "days_in previous_month", "start_days")
+
+month_schema = MonthSchema()
+multiple_month_schema = MonthSchema(many=True)
+
+
+class ReminderScheme(ma.Schema):
+    class Meta:
+        fields = ("id", "day",  "month", "year", "text")
+
+        reminder_schema = ReminderSchema()
+
+multiple_reminder_schema = ReminderSchema(many=True)
+month_schema = MonthSchema()
+    
+
 
 
 
