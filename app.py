75e65b1c1151c435e6548f20eb2d8898d20e1277
crystalmays <chrissymays95@gmail.com>
2022-01-24 22:53:55 -0500

    added reminder table

--- app.py
+++ app.py
@@ -26,7 +26,20 @@ class Month(db.Model):
     self.start_day = start_day
 
 
-    
+class Reminder(db.Model)
+id = db.Column(db.Integer, primary_key=True)
+day = db.Column(db.String, nullable = False)
+month = db.Column(db.String, nullable = False)
+year = db.Column(db.String, nullable = False)
+text = db.Column(db.String, nullable = False)
+
+def __init__(self,day,month,year,text):
+    self.day = day
+    self.month = month
+    self.year = year
+    self.text = text 
+
+
 
 
 if __name__ == "__main__":
